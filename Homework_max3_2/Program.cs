277ca1361ce7106189c317d0699e37a70142dbbc
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-13 00:12:05 +0300

    add max 3_2

---
+++ Homework_max3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 22;
+        int b = 3;
+        int c = 9;
+
+        int max = a;
+
+        if (a > max) max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        Console.Write("max = ");
+        Console.WriteLine(max);
+    }
+}
