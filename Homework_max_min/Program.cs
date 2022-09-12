20e642f1bf7ad3317ff61099a4a0d710985b067c
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-12 23:28:44 +0300

    max_min

---
+++ Homework_max_min/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 5, b = 7;
+
+Console.WriteLine("min = 5, max = 7");
+
+int c = 2, d = 10;
+
+Console.WriteLine("min = 2, max = 10");
+
+int e = -9, f = -3;
+
+Console.WriteLine("min = -9, max = -3");
