a971f6c93be96ec3b5eb3f5f674456ae4697917b
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-13 12:49:13 +0300

    add div

---
+++ Homework_div/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;                             
+            Console.Write("введите число: ");
+            a= Console.Read();
+ 
+            if (a % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+ 
+            Console.ReadKey();
+        }
+    }
+}   
